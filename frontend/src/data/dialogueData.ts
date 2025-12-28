62933cda708d59ab52028f6340334c62dc421bd8
ludson96 <ludson_ps27@hotmail.com>
2025-12-28 11:46:42 -0300

    feat: :sparkles: add data for consume

---
+++ frontend/src/data/dialogueData.ts
@@ -0,0 +1,50 @@
+export const PROLOGO = [
+  "A Jornada de Hot Dog",
+  "📜 Passado...",
+  "Vindo de onde os olhos não conseguiam alcançar, com um som alto passando pelas montanhas e vales, pelos rios e florestas. Todos no mundo puderam ouvir uma voz que parecia um estrondo, que dizia:",
+  "Contemplem seu novo mestre, Glozium..." ,
+  "<strong>Demonstrativus potentiaaam!!!</strong>",
+  "O terro re caiu sobre as pessoas e o mundo foi tomadao por uma neblina que provocou todo tipo de mazela. Alguns morreram, outros tornaram-se monstros, animais sofreram mutações. Mas após a dissolução dessa terrível onda mortífera, era possível ver em meio a neblina o que pareciam estátuas em pé, onde o som foi mais intenso...",
+  "⏳ Agora...",
+  "A província de <strong>Hospitalis</strong> é conhecida por seus médicos mágicos poderosos, que foram capazes de erradicar uma doenã terrível lançada pelo grande vilão Glozium.",
+  "Infelizmente, os hospitalienses não conseguem lidar com todo o poder de Glozium sozinhos e, por isso, a província depende de guerreiros estrangeiros para ajudá-los a se defender. A famosa <strong>Zerum Glozium</strong> é a principal Guilda do reino, seus ferreiros desenvolvem armas para os guerreiros capazes de travar embates contra as forças de Glozium. Esses guerreiros escolhidos são chamados de <strong>Analyticaes di Glozium</strong>.",
+  "Para reduzir os impactos do monstro imortal Glozium, que ressurge a cada ano, a província de Hospitalis realiza um grande ritual chamado <strong>Ciclum Receitatus Hospitalis</strong>. Esse ritual pode dar a um guerreiro condições de destruir Glozium, reduzindo seu poder a zero. Mesmo que não seja destruído, ele é sempre enfraquecido e somente recobra seus poderes no póximo ano.",
+  "Para que o ritual se concretize, o guerreiro escolhido deve partir em uma jornada visitando quatro locais para obter os itens necessários. E <strong>Hot Dog</strong> parte nessa jornada..."
+
+]
+
+export const STAGES = [
+  {
+    id: "floresta-do-atendimentus",
+    title: "Floresta do Atendimentus"
+  },
+  {
+    id: "cavernas-de-faturamentus",
+    title: "Cavernas de Faturamentus"
+  },
+  {
+    id: "vila-da-tramssao",
+    title: "Vila da Transmissão"
+  },
+  {
+    id: "torre-de-contas-a-receber",
+    title: "Torre de Contas a Receber"
+  },
+  {
+    id: "final-battle",
+    title: "Batalha contra Glozium (final)"
+  },
+]
+
+export const FLORESTA = [
+  "Um lugar encantado, dizem que pessoas doentes ou com almas feridas podem ir a floresta para serem curadas",
+  "A floresta parece mais sinistra que nunca, Hot Dog percorre lugares combrios e neblinados enquanto fala com seu pai Sandubinha, que intrui telepaticamente:",
+  "<i>- Sandubinha - na floresta de Atendimentus, encontre o ser \"Processus\" ele irá te mostrar o monstro a ser enfrentado.</i>",
+  "Porém Hot Dog se perde na floresta, mas de repente seu cachorro fareja algo e começa a correr, o herói o segue pois confia totalmente me seu querido animal.",
+  "mergulhando nos arbustos de espinhos que ragam a pele, eles correm até um campo limpo e se deparam com algo terrível...",
+  "Processus estava morto, caído aos pés de uma criatura humanoide com um olhar nojento e arrogante, como um demônio:",
+  "- Hot Dob - não há dúvidas, você é o inimigo, como você feriu o Processus? não era para seu poder afetar seres mágicos do tipo dele!",
+  "- Monstro - não afeta diretamente, mas interfere no equilíbrio, eu impeço o atendimento das almas, eu sou <strong>Anti-authorizatus!</strong> e mesmo esses seres precisam passar pelo Ciclo de Hospitales HAHAHA que pena, esse já era, agora é sua vez e depois matarei esse pirralho assustado nos arbustos, filho desse aqui",
+  "- Hot Dog - não se preocupe jovem, irei me vingar pelo amigo de meu pai!",
+  "Uma batalha sangrenta se inicia..."
+]
